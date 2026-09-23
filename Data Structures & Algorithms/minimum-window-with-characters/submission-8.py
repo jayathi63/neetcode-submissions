e83ff36d3527f3339cc2e32029006b02f6800056
jayathi63 <70006605+jayathi63@users.noreply.github.com>
2026-09-23 08:59:53 +0530

    Add: minimum-window-with-characters - submission-8

---
+++ Data Structures & Algorithms/minimum-window-with-characters/submission-8.py
@@ -0,0 +1,75 @@
+class Solution:
+    def minWindow(self, s: str, t: str) -> str:
+
+        len_s = len(s)
+        len_t = len(t)
+
+        if len_s < len_t:
+            return ""
+        if s == t:
+            return s
+
+        map_t = dict()
+        window_map = dict()
+        required = 0
+
+        for i in t:
+            map_t[i] = map_t.get(i,0) + 1
+            window_map[i] = 0
+        
+        required = len(map_t)
+
+        
+
+        i = 0
+        j = 0
+        n = len(s)
+        min_v = ""
+
+        def is_equal(window_map,map_t):
+
+            for i in map_t.keys():
+                if window_map[i] < map_t[i]:
+                    return False
+            
+            return True
+        formed = 0
+        seen = set()
+        while(j < n):
+            
+            while(j < n and formed != required):
+                if s[j] in window_map:
+                    window_map[s[j]] += 1
+                    if window_map[s[j]] >= map_t[s[j]] and s[j] not in seen:
+                        formed+=1
+                        seen.add(s[j])
+                    # print(s[j],j)
+                    # print(window_map,map_t,formed,required,s[i:j])
+                j+=1
+                
+            # print("ccccccccccccc")
+            # print(window_map,map_t,formed,required,s[i:j])
+            
+            while(i < n and formed == required):
+                if s[i] in window_map:
+                    window_map[s[i]] -= 1
+                    if window_map[s[i]] < map_t[s[i]]:
+                        formed-=1
+                        seen.remove(s[i])
+                i+=1
+            # print(window_map,map_t,formed,required,s[i-1:j])
+            if i-1>=0 and j >= 0:
+                min_v = min_v if min_v and (len(min_v) < j-(i-1)) else s[i-1:j]
+            
+            # print(min_v)
+            
+            
+            
+        
+        return min_v
+
+        
+
+        
+
+        
